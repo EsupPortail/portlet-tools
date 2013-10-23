7d1b0ca98b6d8a897c740de38804f4f5949c9086
Grégoire Neuville <gregoire.neuville@gmail.com>
2013-10-23 18:37:47 +0200

    In due course ; not fully operational yet.

---
+++ portlet-prototyping-maven-plugin/src/main/java/org/esupportail/portlet/prototyping/maven/Library.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010 Johannes Lehtinen 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.esupportail.portlet.prototyping.maven;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Container for library artifact identity information. Mostly exists to be able use proper
+ * element name in configuration.
+ */
+public class Library extends ArtifactIdentity {
+	
+	/**
+	 * Constructs a new uninitialized instance.
+	 */
+	public Library() {
+		super();
+	}
+	
+	/**
+	 * Constructs a new instance and initializes it with the specified data.
+	 * 
+	 * @param groupId group identifier
+	 * @param artifactId artifact identifier
+	 * @param version version number
+	 */
+	public Library(String groupId, String artifactId, String version) {
+		super(groupId, artifactId, version, "jar");
+	}
+
+	/**
+	 * @see ArtifactIdentity#validate()
+	 */
+	public void validate() throws MojoExecutionException {
+		super.validate();
+		if (file == null) {
+			if (!"jar".equals(getPackaging())) {
+				throw new MojoExecutionException("type must be jar");
+			}
+		}
+	}
+
+}
